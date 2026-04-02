c9c9727db270af60696ea1213cb1b94f3eaa1e52
matapaku <matapaku0119@gmail.com>
2026-04-02 09:36:54 +0900

    エラー修正

--- src/components/ApplicationLink/ApplicationLink.tsx
+++ src/components/ApplicationLink/ApplicationLink.tsx
@@ -23,7 +23,6 @@ const ApplicationLink: React.FC = () => {
                 src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"
                 alt="友だち追加"
                 height="36"
-                border={0}
               />
             </a>
           </div>
